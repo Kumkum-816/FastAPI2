707fab7c1c377c0e869617f2bf5da9ff1c47f30b
Kumkum-816 <137768081+Kumkum-816@users.noreply.github.com>
2025-03-19 14:21:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ app = FastAPI()
 
 # Connect to MongoDB
 client = MongoClient("mongodb+srv://kumkumpatel64:0SMHm4HtXy88ZPB9@cluster0.xysjt.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-db = client.key_value_database
-collection = db.data
+db = client.key_value_store
+collection = db.store
 
 # Pydantic Model for Data Validation
 class DataItem(BaseModel):
